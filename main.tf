a837656b81f7619c94ee1e1e74d58d9744a697ac
neogeno <boss@neogeno.com>
2021-04-18 20:51:26 +0800

    First push

---
+++ main.tf
@@ -0,0 +1,103 @@
+terraform {
+  required_version = "~> 0.14"
+
+  provider_meta "terradraw" {
+    appname = "Sample Web Service"
+  }
+}
+
+provider "aws" {
+  region = "ap-east-1"
+}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*",
+        "lambda:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = [aws_iam_role.iam_for_lambda.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "testpy.zip"
+  function_name = "lambda_in_vpc"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+  
+  vpc_config {
+      subnet_ids  = [aws_subnet.mysubnet.id]
+      security_group_ids = [ aws_security_group.allow_tls.id ]
+  }
+
+  source_code_hash = filebase64sha256("testpy.zip")
+
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_iam_role" "another_role" {
+  name = "another_role_name"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
